bf03fee99b6584fe48563e3d2389ded8d026bab4
LLEEVV2020 <lleevv2020@gmail.com>
2019-07-21 19:52:30 +0400

    add

---
+++ Шаблонные файлы/php-file/main/es18_bg22.php
@@ -0,0 +1,16 @@
+<div class="es18_bg">
+            <div class="container">
+
+                <div class="flex-container">
+
+                    <h3>ШКАФЫ И КУХНИ ПО НИЗКИМ ЦЕНАМ </h3>
+                    <i class="angle-angle"></i>
+                    <a href="//<?php echo $_SERVER['SERVER_NAME']; ?>">Шкафы цены</a>
+                    <a href="//<?php echo $_SERVER['SERVER_NAME']; ?>">Кухни цены</a>
+
+                </div>
+
+
+            </div>
+
+        </div><!-- /.es18_bg -->
